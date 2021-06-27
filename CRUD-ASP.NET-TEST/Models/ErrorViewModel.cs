710b9fd2a846b1f116718108246569675a434972
Albandry AlQaseemi <Albandr.alqaseemi@gmail.com>
2021-06-27 14:04:42 +0300

    commit

---
+++ CRUD-ASP.NET-TEST/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_ASP.NET_TEST.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
